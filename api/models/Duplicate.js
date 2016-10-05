3c011dc4e9e34f24023b29ff3127c373459d9f89
Andres Barradas <arbadev@gmail.com>
2016-10-05 18:00:30 -0400

    added compare service

--- api/models/Duplicate.js
+++ api/models/Duplicate.js
@@ -18,12 +18,17 @@ export default class Duplicate extends Model {
   }
 
   /**
-  *
-  *
+  * @method create
+  * @description create a document with numberA and numberB but if this alredy
+  * exist just update its quantity
+  * @param numberA = String
+  * @param numberB = String
+  * @author Andres Barradas
   */
   static create(numberA, numberB) {
     const { CompareService } = proton.app.services
     const areDuplicates = CompareService.areDuplicates(numberA, numberB)
+    proton.log.debug('areDuplicates', areDuplicates)
     const validInput = true
     const criteria = { numberA, numberB }
     const opts = { new: true, upsert: true }
--- api/services/CompareService.js
+++ api/services/CompareService.js
@@ -1,17 +1,31 @@
 'use strict'
 import Service from 'proton-service'
-import fs from 'fs'
-import path from 'path'
+import _ from 'lodash'
 
 export default class CompareService extends Service {
-  // constructor(app) {
-  //   // super(app)
-  //   // this.sg = sg(process.env.SENDGRID_API_KEY)
-  // }
 
+  /**
+  * @method create
+  * @description create a document with numberA and numberB but if this alredy
+  * exist just update its quantity
+  * @param a = String (numberA)
+  * @param b = String (numberB)
+  * @author Andres Barradas
+  */
   areDuplicates(a, b) {
-    if (a === b) return true
-    return false
+    const arrayA = a.split(',')
+    const arrayB = b.split(',')
+    if (arrayA.length !== arrayB.length) return false
+    let flag = true
+    arrayA.forEach(element => {
+      if (_.includes(arrayB, element)) {
+        let index = arrayB.indexOf(element)
+        arrayB.splice(index, 1)
+      }
+      else {
+        flag = false
+      }
+    })
+    return flag
   }
-
 }
